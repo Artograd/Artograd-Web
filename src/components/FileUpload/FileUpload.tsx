0f154fd1f3bf409b18cef5eaf87c8582f035d6dc
Aleksejs Kanarciks <kotik90@gmail.com>
2024-03-15 18:42:03 +0200

    feat(EPMGCIPART-72): reword directory

--- src/components/FileUpload/FileUpload.tsx
+++ src/components/FileUpload/FileUpload.tsx
@@ -57,7 +57,7 @@ export const FileUpload = ({
           newAttachments.push(newFile);
           uuiApi
             .uploadFile(
-              ORIGIN.concat(`/uploadFile/${filesDirectoryId}/subFolder`),
+              ORIGIN.concat(`/uploadFile/tenderFolder/${filesDirectoryId}`),
               file,
               {
                 onProgress: (progress) => trackProgress(progress, tempId),
